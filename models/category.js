6914731a2199a5bfcd5ce94e68733ff15a73dedf
Alyoshka <alyoshkamyshko@gmail.com>
2023-08-30 08:51:26 -0700

    Update category schema

--- models/category.js
+++ models/category.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
-    name: { type: String, required: true, maxLength: 50 },
+    name: { type: String, required: true, minLength: 2, maxLength: 50 },
     description: { type: String, maxLength: 200 }
 });
 
